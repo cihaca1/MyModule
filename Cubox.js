bf70b4231dfc262b95bd247fe8f8156927c42058
cihaca1 <112849097+cihaca1@users.noreply.github.com>
2025-11-25 14:55:12 +0800

    Delete Cubox.js

--- Cubox.js
+++
@@ -1,5 +0,0 @@
-[rewrite_local]
-^https?:\/\/cubox\.pro\/c\/api\/(userInfo|userPay) url script-response-body https://raw.githubusercontent.com/cihaca1/MyModule/main/cubox.js
-
-[mitm] 
-hostname = cubox.pro
